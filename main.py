dd707bf2e027c7ac3d237904d646f87e1f89764d
Muhamad Faishal Irawan <muhammadfaishal193@gmail.com>
2023-10-12 16:25:27 +0700

    move corrections to json

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-from sql_app import crud, models, schemas
-from sql_app.database import SessionLocal, engine
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 import cutlet
 import pysbd
-
-models.Base.metadata.create_all(bind=engine)
+import json
 
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class Song(BaseModel):
+    uri: str
+    lyrics: str
+    provider: str
 
 
-@app.post("/api/transliterate", response_model=schemas.Song)
-async def transliterate_lyrics(song: schemas.SongBase, db: Session = Depends(get_db)):
-    db_song = crud.get_song_by_uri(db, uri=song.uri)
-    if db_song:
-        return db_song
-        # raise HTTPException(status_code=400, detail="Song already registered")
-
+@app.post("/api/transliterate")
+async def transliterate_lyrics(song: Song):
     katsu = cutlet.Cutlet()
     katsu.use_foreign_spelling = False
     katsu.add_exception("♪", "♪")
@@ -48,15 +38,10 @@ async def transliterate_lyrics(song: schemas.SongBase, db: Session = Depends(get
     tl_lyrics = song.lyrics.replace(", ", "、").replace(",", "、")
     tl_lyrics = romajify(tl_lyrics)
 
-    # if song.uri == "spotify:track:2U6mFmBDjaAu6oCCDRpRet":
-    #     tl_lyrics = tl_lyrics.replace("hirakanu", "akanu")
-    # elif song.uri == "spotify:track:7fKFmrw1RSwU5a9vCwk155":
-    #     tl_lyrics = tl_lyrics.replace("Korourai", "Kororon")
-    #     tl_lyrics = tl_lyrics.replace("oto", "ne")
-    #     tl_lyrics = tl_lyrics.replace("1 kai", "ikkai")
-    #     tl_lyrics = tl_lyrics.replace("Suutaryoku", "Kazu amata")
-    #     tl_lyrics = tl_lyrics.replace("kne", "koto")
-    #     tl_lyrics = tl_lyrics.replace("Kneba", "Kotoba")
-    #     tl_lyrics = tl_lyrics.replace("shounenjou", "shounenba")
-    #
-    return crud.create_song(db=db, song=song, tl_lyrics=tl_lyrics)
+    with open("corrections.json") as f:
+        d = json.load(f)
+        if song.uri in d and song.provider in d[song.uri]:
+            for correction in d[song.uri][song.provider]:
+                tl_lyrics = tl_lyrics.replace(correction[0], correction[1])
+
+    return tl_lyrics
